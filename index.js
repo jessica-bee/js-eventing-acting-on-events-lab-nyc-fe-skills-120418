93a6ff9f20550d30bd82e24f0b64cb368be39e5f
Jessica Bee <bee.jess@gmail.com>
2019-01-30 01:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const moveDodgerLeft = () => {
   const currentLeft = dodger.style.left
   let currentLeftNumber = currentLeft.replace('px', '')
   currentLeft - parseInt(currentLeftNumber, 10)
-  dodger.style.left = currentLeftNumber - 1 + 'px'
+  dodger.style.left = currentLeftNumber - 5 + 'px'
 }
 
 const moveDodgerRight = () => {
-  
+  const dodger = document.getElementById ('dodger')
+  const currentRight = dodger.style.left
+  let currentRightNumber = currentLeft.replace('px', '')
+  currentRight - parseInt(currentRightNumber, 10)
+  dodger.style.left = currentRightNumber + 5 + 'px'
 }
 
 const moveDodgerHelper = (event) => {
   if (event.keyCode === 37) {
     moveDodgerLeft()
-  } 
+  } if (event.keyCode === 39){
+    moveDodgerRight()
+  }
 }
 
 document.addEventListener('keydown', moveDodgerHelper)
