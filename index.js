fd0d26e902f3abe23d9c5148badacd5c77bf809b
Jessica Bee <bee.jess@gmail.com>
2019-01-30 01:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moveDodgerLeft = () => {
   const dodger = document.getElementById ('dodger')
   const currentLeft = dodger.style.left
-  const currentLeftNumber = currentLeft.replace('px', '')
+  let currentLeftNumber = currentLeft.replace('px', '')
   currentLeft - parseInt(currentLeftNumber, 10)
   dodger.left.style = currentLeftNumber - 1 + 'px'
 }
